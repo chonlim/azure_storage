fc585434bff2bd4c413a9ef39ab3cd6403d2d9a5
chonlim <111695408+chonlim@users.noreply.github.com>
2022-08-24 21:39:36 +0200

    Add code to repo

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'udacity-exercise-sqlserver.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'udacity-exercise-sqldb'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'chonlim'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'HDmap_validation88'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+18+for+SQL+Server'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'satestudacitynanodegree'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'j9t8uqPe6ARb3TGkJ6Dy1XEyCSHWS+PbnGxGdiI92SSUwXtdXQ4hVawGkKpMrQWumISNmDjDy5Xw+AStBBO8eA=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
